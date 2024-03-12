3ef4b9e432a2f7b75c493dfa5daea18befbd07fd
liliaferrouk <ferrouklilia@gmail.com>
2024-03-12 22:16:23 +0100

    organiser l'espace de travail

---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer>
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+        Coded by <a href="https://github.com/liliaferrouk">Lilia Ferrouk</a>.
+    </footer>
+  )
+}
+
+export default Footer
