8d3646cc2b449bf2e0d5f6bd38d6ba2bed3b329e
liliaferrouk <ferrouklilia@gmail.com>
2024-03-15 16:16:51 +0100

    added the logique working

---
+++ src/FAQ.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react'
+import icon_plus from './images/icon-plus.svg'
+import icon_minus from './images/icon-minus.svg'
+
+function FAQ(props) {
+    const [isShowen,setIsShowen] = useState(false)
+    function toggleFAQ(){
+        setIsShowen(!isShowen)
+    }
+  return (
+    <div className='FAQ'>
+        <div className='question'>
+          <h2>{props.question}</h2>
+          <img onClick={toggleFAQ} width={'30px'} src={isShowen ? icon_minus : icon_plus} alt="icon_minus" />
+        </div>
+        {isShowen && <p>{props.reponse}</p>}
+      </div>
+  )
+}
+
+export default FAQ
